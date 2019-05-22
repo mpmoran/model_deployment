a8105dd4bfa204f39a9ad615bc826b6b0d57b30e
Michael P. Moran <mmutexas@gmail.com>
2019-05-21 19:02:12 -0500

    add answers to exercises

--- model.py
+++ model.py
@@ -3,9 +3,8 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, accuracy_score
 
-df = pd.read_csv('./spam_clean.csv')
+df = pd.read_csv("./spam_clean.csv")
 df.head()
 
 tfidf = TfidfVectorizer()
@@ -13,9 +12,12 @@ tfidf = TfidfVectorizer()
 X = tfidf.fit_transform(df.text)
 y = df.label
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=.2)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, stratify=y, test_size=0.2
+)
 
-lm = LogisticRegression(solver='saga').fit(X_train, y_train)
+lm = LogisticRegression(solver="saga").fit(X_train, y_train)
+
 
 def predict(msg):
     return lm.predict(tfidf.transform([msg]))[0]
--- server.py
+++ server.py
@@ -1,14 +1,58 @@
 import random
-from flask import Flask
+from flask import Flask, render_template, request
+from model import predict
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    return render_template("index.html")
 
 
 @app.route("/roll-dice")
 def roll_dice():
-    return str(random.randint(1, 6))
+    return render_template(
+        "roll_dice.html", dice_roll_result=random.randint(1, 6)
+    )
+
+
+@app.route("/my-first-form")
+def my_first_form():
+    return render_template("my-first-form.html")
+
+
+@app.route("/make-greeting", methods=["POST"])
+def handle_form_submission():
+    name = request.form["name"]
+    title = request.form["title"]
+
+    greeting = "Hello, "
+
+    if title != "":
+        greeting += title + " "
+
+    greeting += name + "!"
+
+    return render_template("greeting.html", greeting=greeting)
+
+
+@app.route("/spam-detector")
+def spam_detector():
+    return render_template("spam-detector.html")
+
+
+@app.route("/classify-message", methods=["POST"])
+def classify_message():
+    msg = request.form["message"]
+
+    result = predict(msg)
+    text_type = "warning"
+    if result == "ham":
+        text_type = "success"
+    elif result == "spam":
+        text_type = "danger"
+
+    return render_template(
+        "classify-message.html", text_type=text_type, result=result
+    )
